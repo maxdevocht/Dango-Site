0cecf0ec11471c4c6968932cdea2f3a1c565a3fb
Max de Vocht <maxdevocht@hotmail.com>
2024-06-13 17:56:28 +0200

    Created Navbar.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='bg-[#EBEBEB]'>
+      <Navbar />
+    </main>
   )
 }
 
