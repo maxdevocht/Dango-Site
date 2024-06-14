e90cf251ead92be50886cd1d5bd72f2276a6a6cb
Max de Vocht <maxdevocht@hotmail.com>
2024-06-14 12:12:39 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,12 @@
-
-import { batteryCameraVid } from '../utils';
+import React from 'react'
 
 const Hero = () => {
-
-      
   return (
-    <section className="w-full nav-height bg-[#E9E9E9] relative">
-      <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">Dango</p>
-        <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={batteryCameraVid}>
-            <source src={batteryCameraVid} type="video/mp4" />
-          </video>
-        </div>
-      </div>
-
-      <div
-        id="cta"
-        className="flex flex-col items-center opacity-0 translate-y-20"
-      >
-        <a href="#highlights" className="btn">Buy</a>
-        <p className="font-normal text-xl">From $199/month or $999</p>
-      </div>
+    <section>
+        Dango
     </section>
   )
 }
 
 export default Hero
+
