b6decaa50114725dddceead94a279c3adbd25b09
Max de Vocht <maxdevocht@hotmail.com>
2024-06-15 10:58:04 +0200

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { navList } from '../constants'
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-center items-center">
-        <nav className="flex max-sm:w-full">
-            <a href='#' className='mr-1 px-1 py-1 bg-white bg-opacity-45 backdrop-filter backdrop-blur-lg border border-gray border-opacity-25 rounded-full'>
+        <nav className="flex fixed z-50 max-sm:w-full mt-10">
+            <a href='#' className='mr-1 px-1 py-1 bg-gray bg-opacity-20 backdrop-filter backdrop-blur-lg border border-white border-opacity-25 rounded-full'>
               <img src='/assets/images/logo_dark.svg' alt="logo" width={24} height={24}/>
             </a>
 
-            <div className='flex flex-1 gap-6 px-5 py-1 max-sm:hidden bg-white bg-opacity-45 backdrop-filter backdrop-blur-lg border border-gray border-opacity-25 rounded-lg'>
+            <div className='flex flex-1 items-center gap-6 px-5 py-1 max-sm:hidden bg-gray bg-opacity-20 backdrop-filter backdrop-blur-lg border border-white border-opacity-25 rounded-lg'>
               {navList.map((nav) => (
-                <div href='#' key={nav} lassName="px-5 text-sm cursor-pointer text-gray-100 transition-all hover:text-zinc transition-all">{nav}</div>
+                <a href='#' key={nav} className="px-2 text-sm text-zinc hover:text-gray transition-all">{nav}</a>
               ))}
             </div>
         </nav>
