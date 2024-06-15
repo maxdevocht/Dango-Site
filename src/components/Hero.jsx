94d06001e4be35978d84c9fd804721a11158fb3a
Max de Vocht <maxdevocht@hotmail.com>
2024-06-15 11:08:55 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,17 @@ const Hero = () => {
 
   useGSAP(() => {
     gsap.to('#hero', { opacity: 1, delay: 1.5 })
+    gsap.to('#span', { opacity: 1, delay: 2})
   }, [])
 
   return (
     <section className="w-full relative">
         <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">Dango</p>
-        <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted loop playsInline={true}>
-            <source src="./assets/images/camera_vid.mp4"/>
+        <p id="hero" className="hero-title font-code">Dango.</p>
+        <p id='#span' className='hero-title'>more than a camera</p>
+        <div className="md:w-10/12 w-full flex items-center justify-center">
+          <video className="pointer-events-none" width={450} height={450} autoPlay muted loop playsInline={true}>
+            <source src="./assets/images/camera_vid.mp4" type='video/mp4'/>
           </video>
         </div>
       </div>
